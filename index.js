5b7aeaffc96dcb03bb01b9142b5525e33fd0feda
Daria <des.hope03@gmail.com>
2023-07-14 00:07:33 +0200

    feature: add save user input

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 // - создать одну функцию, которая будет креэйтить 
 // ключ (имя ключа - id) и записывать в него значение
 // - візіваем єту функцию для аждого инпута
+let inputEmaleElem = document.getElementById('email');
+inputEmaleElem.value = localStorage.getItem('email');
+
+function saveInputValue(id) {
+    let elem = document.getElementById(id);
+    localStorage.setItem(id, elem.value);
+    console.log('elem.value',elem.value);
+}
