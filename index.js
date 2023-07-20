77f465f20e1b711fe2626620148eca2a581654cf
Daria <des.hope03@gmail.com>
2023-07-20 13:08:25 +0200

    feature: i18n

--- index.js
+++ index.js
@@ -18,3 +18,44 @@ function saveInputValue(id,type) {
     localStorage.setItem(id, elem.value);
 }
 
+let languageLabelContent = {
+    'en': {
+        'label-language': 'Language',
+        'label-email':'Your email',
+        'label-password': 'Your password',
+        'label-country': 'Your country',
+        'label-notifications': 'Receive notifications about promotions',
+        'label-ua': 'Ua',
+        'label-en': 'En',
+        'label-Ukraine' : 'Ukraine',
+        'label-Belarus': 'Belarus',
+        'label-Poland': 'Poland',
+        'label-Spain': 'Spain',
+        'label-submit': 'Submit'
+    },
+    'ua': {
+        'label-language': 'Мова',
+        'label-email':'Ваша почтова скриня',
+        'label-password': 'Ваш пароль',
+        'label-country': 'Ваша країна',
+        'label-notifications': 'Отримувати повідомлення про акціЇ',
+        'label-ua': 'Укр',
+        'label-en': 'Англ',
+        'label-Ukraine' : 'Україна',
+        'label-Belarus': 'Білорусь',
+        'label-Poland': 'Польща',
+        'label-Spain': 'Іспанія',
+        'label-submit': 'Відправити'
+    }
+}
+
+changeLanguage();
+function changeLanguage() {
+    let languageValue = document.getElementById('select-language').value
+    let labelEls = document.querySelectorAll('[translate]');
+    for (let i = 0; i < labelEls.length; i++ )  { 
+        let translationKey = labelEls[i].getAttribute('translate');
+        let translationValue = languageLabelContent[languageValue][translationKey];
+        labelEls[i].textContent = translationValue;
+    }
+}    
