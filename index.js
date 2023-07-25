1c587dc6a987982e7fd61294408490bd7fbc7880
Daria <des.hope03@gmail.com>
2023-07-25 12:33:15 +0200

    refactor: use module pattern

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-
-let inputEmaleElem = document.getElementById('email');
-inputEmaleElem.value = localStorage.getItem('email');
-let selectCountryElem = document.getElementById('select-country');
-selectCountryElem.value = localStorage.getItem('select-country');
-let formCheckboxElem = document.getElementById('notifications');
-const fromLS = localStorage.getItem('notifications');
-formCheckboxElem.checked = JSON.parse(fromLS);
-
-function saveInputValue(id,type) {
-    if(type === 'checkbox'){
-        let elem = document.getElementById(id);
-        localStorage.setItem(id, elem.checked);
-        return;
-    }
-    console.log('vhod');
-    let elem = document.getElementById(id);
-    localStorage.setItem(id, elem.value);
-}
-
-let languageLabelContent = {
+const LANGUAGE_LABEL_CONTENT = {
     'en': {
         'label-language': 'Language',
         'label-email':'Your email',
@@ -49,13 +29,64 @@ let languageLabelContent = {
     }
 }
 
-changeLanguage();
-function changeLanguage() {
-    let languageValue = document.getElementById('select-language').value
-    let labelEls = document.querySelectorAll('[translate]');
+const store = (() => {
+
+init();
+
+function init() {
+    const inputEmailElem = document.getElementById('email');
+    inputEmailElem.value = localStorage.getItem('email');
+    const selectCountryElem = document.getElementById('select-country');
+    selectCountryElem.value = localStorage.getItem('select-country');
+    const formCheckboxElem = document.getElementById('notifications');
+    const restoredNotificationsCheckboxValue = localStorage.getItem('notifications');
+    formCheckboxElem.checked = JSON.parse(restoredNotificationsCheckboxValue);
+    const restoredLang = localStorage.getItem('select-language');
+    changeLanguage(restoredLang);
+}
+
+function changeLanguage(lang) {
+    translateContent(lang);
+    saveLanguage(lang);
+    setValueForLangSelects(lang);
+}
+
+function translateContent(lang) {
+    const labelEls = document.querySelectorAll('[translate]');
     for (let i = 0; i < labelEls.length; i++ )  { 
-        let translationKey = labelEls[i].getAttribute('translate');
-        let translationValue = languageLabelContent[languageValue][translationKey];
+        const translationKey = labelEls[i].getAttribute('translate');
+        const translationValue = LANGUAGE_LABEL_CONTENT[lang][translationKey];
         labelEls[i].textContent = translationValue;
     }
-}    
+}
+
+function saveLanguage(lang) {
+    localStorage.setItem('select-language', lang);
+}
+
+function setValueForLangSelects(languageValue) {
+    const allLangElems = document.getElementsByClassName('lang-select');
+    for (let i = 0; i < allLangElems.length; i++) {
+        allLangElems[i].value = languageValue;
+    }
+}
+
+function saveInputValue(event) {
+    const currentFormElement = event.target;
+    let value = currentFormElement.type === 'checkbox' ? currentFormElement.checked : currentFormElement.value;
+    localStorage.setItem(currentFormElement.id, value);
+}
+
+function onChangeLanguage(event) {
+    const selectLangElem = event.target;
+    const languageValue = selectLangElem.value;
+    changeLanguage(languageValue);
+}
+
+return {
+    saveInputValue: saveInputValue,
+    onChangeLanguage: onChangeLanguage
+}
+})();
+console.log(store)
+
